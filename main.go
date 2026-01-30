1850d56cd8f82e6a200060be8fdbb6305e1fd8c7
bulsond <bulsond@gmail.com>
2026-01-30 18:29:48 +0300

    step: load banks data

--- main.go
+++ main.go
@@ -1,26 +1,74 @@
 package main
 
 import (
+	"bufio"
+	"errors"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
 
-func main() {
-	fmt.Println("Привет мир")
-
-	fmt.Println(Bank{})
-	fmt.Println(Bank{Name: "LunarBank", BinFrom: 123456, BinTo: 1234567890})
-
-	bytes, err := os.ReadFile("banks.txt")
-	if err != nil {
-		panic(err)
-	}
-	text := string(bytes)
-	fmt.Println(text)
-}
-
+// Bank
 type Bank struct {
 	Name    string
 	BinFrom int
 	BinTo   int
+}
+
+// loadBankData
+func loadBankData(path string) ([]Bank, error) {
+	banks := []Bank{}
+
+	file, err := os.Open(path)
+	if err != nil {
+		return banks, err
+	}
+	defer file.Close()
+
+	const errMessage = "Неверный формат данных в файле банков"
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		txt := scanner.Text()
+		line := strings.TrimSpace(txt)
+		if line == "" {
+			continue
+		}
+
+		parts := strings.Split(line, ",")
+		if len(parts) != 3 {
+			return banks, errors.New(errMessage)
+		}
+
+		name := strings.TrimSpace(parts[0])
+		binFrom, err := strconv.Atoi(strings.TrimSpace(parts[1]))
+		if err != nil {
+			return banks, err
+		}
+		binTo, err := strconv.Atoi(strings.TrimSpace(parts[2]))
+		if err != nil {
+			return banks, err
+		}
+
+		if binFrom > binTo {
+			return banks, errors.New(errMessage)
+		}
+
+		banks = append(banks, Bank{
+			Name:    name,
+			BinFrom: binFrom,
+			BinTo:   binTo,
+		})
+	}
+
+	if err := scanner.Err(); err != nil {
+		return banks, err
+	}
+
+	return banks, nil
+}
+
+func main() {
+	banks, err := loadBankData("banks.txt")
+	fmt.Println(banks, err)
 }
