e51a1d83c68cae3f136336ebd17bd4b8ada735f8
bulsond <bulsond@gmail.com>
2026-01-30 17:15:45 +0300

    step: add banks data file

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
 	fmt.Println("Привет мир")
+
+	fmt.Println(Bank{})
+	fmt.Println(Bank{Name: "LunarBank", BinFrom: 123456, BinTo: 1234567890})
+
+	bytes, err := os.ReadFile("banks.txt")
+	if err != nil {
+		panic(err)
+	}
+	text := string(bytes)
+	fmt.Println(text)
+}
+
+type Bank struct {
+	Name    string
+	BinFrom int
+	BinTo   int
 }
