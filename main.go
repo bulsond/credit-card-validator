d17294fbdc5ac50b2d917e2c79bbf1cd935926f8
bulsond <bulsond@gmail.com>
2026-01-30 23:17:07 +0300

    chore: done identifyBank

--- main.go
+++ main.go
@@ -68,7 +68,30 @@ func loadBankData(path string) ([]Bank, error) {
 	return banks, nil
 }
 
+// extractBin
+func extractBIN(cardNumber string) int {
+	result := 0
+
+	return result
+}
+
+// identifyBank
+func identifyBank(bin int, banks []Bank) string {
+	result := "Неизвестный банк"
+
+	for _, bank := range banks {
+		if bin >= bank.BinFrom && bin <= bank.BinTo {
+			result = bank.Name
+			break
+		}
+	}
+
+	return result
+}
+
 func main() {
 	banks, err := loadBankData("banks.txt")
 	fmt.Println(banks, err)
+
+	fmt.Println(identifyBank(400000, banks))
 }
